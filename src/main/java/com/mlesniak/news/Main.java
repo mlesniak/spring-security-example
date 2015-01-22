de95902d6078d50531d227044dddcd46ead30c1d
Michael Lesniak <mail@mlesniak.com>
2015-01-22 07:18:26 +0100

    Configurable support for users.

--- src/main/java/com/mlesniak/news/Main.java
+++ src/main/java/com/mlesniak/news/Main.java
@@ -1,8 +1,7 @@
 package com.mlesniak.news;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.context.annotation.ComponentScan;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -13,14 +12,20 @@ import org.springframework.web.bind.annotation.ResponseBody;
  * @author Michael Lesniak (mail@mlesniak.com)
  */
 @Controller
-@ComponentScan
-@EnableAutoConfiguration
+@SpringBootApplication
 public class Main {
-    @RequestMapping("/")
     @ResponseBody
-    String home() {
+    @RequestMapping("/")
+    String root() {
         return "Hello World!";
     }
+
+    @ResponseBody
+    @RequestMapping("/admin")
+    String admin() {
+        return "Hello Admin!";
+    }
+
 
     public static void main(String[] args) throws Exception {
         SpringApplication.run(Main.class, args);
---
+++ src/main/java/com/mlesniak/news/SecurityConfiguration.java
@@ -0,0 +1,33 @@
+package com.mlesniak.news;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * @author Michael Lesniak (mail@mlesniak.com)
+ */
+@Configuration
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("admin")
+                   .password("admin")
+                    .roles("ADMIN")
+        ;
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                    .antMatchers("/").permitAll()
+                    .antMatchers("/admin").hasRole("ADMIN")
+                    .and()
+                .httpBasic()
+        ;
+
+    }
+}
